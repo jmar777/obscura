dd65b9c60987f5ddcc7fa5a28e9bbcffaab77e4b
jmar777 <jmar777@gmail.com>
2014-06-21 23:36:56 -0400

    header component

---
+++ src/view/components/header.jsx
@@ -0,0 +1,12 @@
+/** @jsx React.DOM */
+
+var Header = React.createClass({
+	render: function() {
+		return (
+			<div id='header'>
+				<a href='#' className='fa fa-arrow-circle-right'></a>
+				
+			</div>
+		);
+	}
+});
